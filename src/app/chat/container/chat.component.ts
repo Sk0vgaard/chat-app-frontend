df3ab68defc9538cc62de3fd49ca7488d1f7e3b6
Sk0vgaard <sk0vgaard@live.dk>
2021-02-25 20:23:58 +0100

    add unsubscribe for listener

--- src/app/chat/container/chat.component.ts
+++ src/app/chat/container/chat.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ChatService } from '../shared/services/chat.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
 
   messages: string[] = [];
   message = new FormControl('');
+  unsubscribe$ = new Subject();
 
   constructor(private chatService: ChatService) {
   }
 
   ngOnInit(): void {
     this.chatService.messageListener()
+      .pipe(takeUntil(this.unsubscribe$))
       .subscribe(message => {
         this.messages.push(message);
       });
@@ -25,6 +29,11 @@ export class ChatComponent implements OnInit {
   sendMessage(): void {
     console.log(this.message.value);
     this.chatService.sendMessage(this.message.value);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
 }
