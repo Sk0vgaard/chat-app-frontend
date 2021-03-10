c6889fbbb7828fcf8239ef523345b7c5fba52eb5
Sk0vgaard <sk0vgaard@live.dk>
2021-03-10 18:46:27 +0100

    Make message content scrollable

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -11,6 +11,8 @@ import { SharedModule } from '../shared/shared.module';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatLineModule } from '@angular/material/core';
 
 
 @NgModule({
@@ -29,7 +31,9 @@ import { MatIconModule } from '@angular/material/icon';
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule,
+    MatLineModule
   ]
 })
 export class ChatModule {
